ccb87b2a51a9c84b636379c0936a002d4667d5a1
Vasudeva <devakingvasudeva@gmail.com>
2025-12-11 15:09:28 +0530

    ready for deploy

--- backend/todo_app/settings.py
+++ backend/todo_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r_&0k&1@o(cnum376t7uo=-k22_$0!mopat=cz!i@x8t+abzh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,4 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# This tells Django where to put collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: if you have additional static folders
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # your project-level static folder
+]
